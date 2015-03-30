0d020ec3dc6b6f630cea01291f00f74b806d9e49
so-olitary <kill143@mail.ru>
2015-03-30 22:02:43 +0300

    last_fix

--- epoll_fix.cpp
+++ epoll_fix.cpp
@@ -15,6 +15,9 @@
 #define USR_LIMIT 31
 #define MSG_LIMIT 1025
 
+// #define _DEBUG
+// #define _DEBUG_1
+
 int set_nonblock (int fd)
 {
     int flags;
@@ -99,8 +102,14 @@ int main (int argc, char **argv)
         // куча дескрипторов и мы ждём разных событий, которые не должны быть однородны.
         // Мы не обязаны писать один код для всех случаев, но нам нужна дифференциация...
         int N = epoll_wait (EPoll, Events, MAX_EVENTS, -1);
+#ifdef _DEBUG_1
+        std::cout << "Num of Events: " << N << "\n" << std::endl;
+#endif
         for ( unsigned int i = 0; i < N; i++ )
         {
+#ifdef _DEBUG_1
+            std::cout << "Inside iteration: " << i << "\n" << std::endl;
+#endif
             if( (Events[i].events & EPOLLERR)
                 || (Events[i].events & EPOLLHUP)
                 || (Events[i].events & EPOLLOUT) )
@@ -108,13 +117,14 @@ int main (int argc, char **argv)
                 // если ошибка - закрываем
                 shutdown (Events[i].data.fd, SHUT_RDWR);
                 close (Events[i].data.fd);
-                std::cout << "connection terminated" << std::endl;
-                //Чистим буфер
-                for( int j = 0; j< MSG_LIMIT; j++)
-                {
-                    user_buffer[i][j]=0; 
-                }
-                user_buffer_len[i]=0;
+                std::cout << "connection terminated by ERROR or HANGUP" << std::endl;
+                // Чистим буфер
+                // for( int j = 0; j< MSG_LIMIT; j++)
+                // {
+                //     user_buffer[i][j]=0; 
+                // }
+                memset (user_buffer[i], 0, MSG_LIMIT);
+                user_buffer_len[i] = 0;
             }
             
             else if ( Events[i].data.fd == MasterSocket )
@@ -158,48 +168,77 @@ int main (int argc, char **argv)
             else
             {
                 char *recv_ptr2nline;
-                char recv_buffer[MSG_LIMIT];
-                int recv_len;
+                char  recv_buffer[MSG_LIMIT];
+                int   recv_len;
                 
                 // читаем из сокета (read без signal)
                 do
                 {
-                    std::cout << "!!!" << std::endl;
                     recv_len = recv (Events[i].data.fd, recv_buffer, (MSG_LIMIT - 1U), MSG_NOSIGNAL);
+#ifdef _DEBUG                    
+                    std::cout << "!!!" << std::endl;
+                    std::cout << "Recv len and recv buffer: ";
+                    std::cout /* << "Here!" << std::endl; */ << recv_len << ' ' << recv_buffer << std::endl;
+                    std::cout << "Error number: ";
+                    std::cout << strerror (errno) << std::endl;
+#endif
+#ifdef _DEBUG_1
+                   std::cout << "Recv Len: "<< recv_len << " Error number: "<< strerror (errno) << std::endl;    
+#endif
+                    
                     if( recv_len == 0 )
                     {
                         std::cout << "Nothing!" << std::endl;
-                        // shutdown (Events[i].data.fd, SHUT_RDWR);
-                        // close (Events[i].data.fd);
-                        // std::cout << "connection terminated" << std::endl;
+                        shutdown (Events[i].data.fd, SHUT_RDWR);
+                        close (Events[i].data.fd);
+                        std::cout << "connection terminated" << std::endl;
                         break;
                     }
                 
                     /* || */
                     if( recv_len == -1 )
                     {
-                        std::cout << "Error!" << std::endl;
-                        // завершаем соединение
-                        shutdown (Events[i].data.fd, SHUT_RDWR);
-                        close (Events[i].data.fd);
-                        std::cout << "connection terminated" << std::endl;
-                        for( int j = 0; j< MSG_LIMIT; j++)
+                        if( errno != EAGAIN )
                         {
-                            user_buffer[i][j]=0; 
-                        }
-                        user_buffer_len[i]=0;
+                            std::cout << "Error!" << std::endl;
+                            // завершаем соединение
+                            shutdown (Events[i].data.fd, SHUT_RDWR);
+                            close (Events[i].data.fd);
+                            std::cout << "connection terminated" << std::endl;
+                            // for( int j = 0; j< MSG_LIMIT; j++)
+                            // {
+                            //     user_buffer[i][j]=0; 
+                            // }
+                            memset (user_buffer[i], 0, MSG_LIMIT);
+                            user_buffer_len[i] = 0;
+                         }
+                         else
+                         {
+                            //TODO Write from buffer
+                            
+                         }
+#ifdef _DEBUG_1
+                
+#endif
                         // continue; //
                         break;
                     }
 
+
                     recv_buffer[recv_len] = '\0';
+#ifdef _DEBUG
+                    std::cout << "Recv len and recv buffer: ";
                     std::cout /* << "Here!" << std::endl; */ << recv_len << ' ' << recv_buffer << std::endl;
                     recv_ptr2nline = (char*) memchr (recv_buffer, '\n', recv_len);
 
                     if ( recv_ptr2nline == NULL )
                         std::cout << ">> NULL\n";
                     else
-                        std::cout << (recv_ptr2nline - recv_buffer); //Debug info
+                    {
+                        std::cout << "\\n found, position: ";
+                        std::cout << (recv_ptr2nline - recv_buffer); // Debug info
+                    }
+#endif
 
                     // парсим сообщения
                     while ( (recv_ptr2nline = (char*) memchr (recv_buffer, '\n', recv_len)) )
@@ -213,26 +252,31 @@ int main (int argc, char **argv)
                             size_t shift = (msg_len + user_buffer_len[i]) - (MSG_LIMIT - 1);
                             recv_ptr2nline -= shift;
                             msg_len -= shift;
+#ifdef _DEBUG
                             std::cout << "shift: " << shift << std::endl;
+#endif
                         }
 
                         memcpy (msg_line, user_buffer[i], user_buffer_len[i]);
-
+#ifdef _DEBUG
                         std::cout << "\n!!! 1 !!!\n";
-                        /* std::cout << ">> b_len: " << user_buffer_len[i] << " buff_i: \"" << user_buffer[i] << "\"" << std::endl;
+                        std::cout << ">> b_len: " << user_buffer_len[i] << " buff_i: \"" << user_buffer[i] << "\"" << std::endl;
                         std::cout << ">> rcv_len: " << recv_len << " recv: \"" << recv_buffer << "\"" << std::endl;
-                        std::cout << ">> msg_len: " << msg_len << " msg: \"" << msg_line << "\"" << std::endl;*/
+                        std::cout << ">> msg_len: " << msg_len << " msg: \"" << msg_line << "\"" << std::endl;
+#endif
 
                         memcpy (msg_line + user_buffer_len[i], recv_buffer, msg_len);
                         msg_len += user_buffer_len[i];
                         user_buffer_len[i] = 0;
                         memmove (recv_buffer, recv_ptr2nline, ((recv_buffer + recv_len) - recv_ptr2nline) );
                         recv_len -= (recv_ptr2nline - recv_buffer);
-                        std::cout << "\n!!! 2 !!!\n";
 
-                        /* std::cout << ">> b_len: " << user_buffer_len[i] << " buff_i: \"" << user_buffer[i] << "\"" << std::endl;*/
-                        /*std::cout << ">> rcv_len: " << recv_len << " recv: \"" << recv_buffer << "\"" << std::endl;*/
-                        /* std::cout << ">> msg_len: " << msg_len << " msg: \"" << msg_line << "\"" << std::endl;*/
+#ifdef _DEBUG
+                        std::cout << "\n!!! 2 !!!\n";
+                        std::cout << ">> b_len: " << user_buffer_len[i] << " buff_i: \"" << user_buffer[i] << "\"" << std::endl;
+                        std::cout << ">> rcv_len: " << recv_len << " recv: \"" << recv_buffer << "\"" << std::endl;
+                        std::cout << ">> msg_len: " << msg_len << " msg: \"" << msg_line << "\"" << std::endl;
+#endif
 
                         msg_line[msg_len] = '\0';
                         std::cout << msg_line << std::endl;
@@ -246,29 +290,31 @@ int main (int argc, char **argv)
                                 shutdown (Events[j].data.fd, SHUT_RDWR);
                                 close (Events[j].data.fd);
                                 std::cout << "connection terminated" << std::endl;
-                                for( int k = 0; k< MSG_LIMIT; k++)
-                                {
-                                    user_buffer[j][k]=0; 
-                                }
-                                user_buffer_len[j]=0;   
+                                // for( int k = 0; k< MSG_LIMIT; k++)
+                                // {
+                                //     user_buffer[j][k]=0; 
+                                // }
+                                memset (user_buffer[j], 0, MSG_LIMIT);
+                                user_buffer_len[j] = 0;   
                             }
                         }
 
                         // msg_line[msg_len] = '\0';
                     // std::cout /*<< "log: " */ << msg_line << std::endl;
                     } // end parsing messages (searching for '\n')
-
+#ifdef _DEBUG
                     // std::cout << "It is here.\n";
                     if ( recv_ptr2nline == NULL)
                         std::cout << ">> NULL\n";
                     else
                         std::cout << (recv_ptr2nline - recv_buffer);
-
+#endif
                     if ( recv_len != 0 )
                     {
                         memcpy (user_buffer[i], recv_buffer, recv_len);
                         user_buffer_len[i] = recv_len;
                         recv_len = 0U;
+                        memset (recv_buffer, 0, MSG_LIMIT);
                     }
 
                     else if( recv_len + user_buffer_len[i] >= MSG_LIMIT )
@@ -282,24 +328,35 @@ int main (int argc, char **argv)
                                 shutdown (Events[j].data.fd, SHUT_RDWR);
                                 close (Events[j].data.fd);
                                 std::cout << "connection terminated" << std::endl;
-                                for( int k = 0; k< MSG_LIMIT; k++)
-                                {
-                                    user_buffer[j][k]=0; 
-                                }
-                                user_buffer_len[j]=0;  
+                                // for( int k = 0; k< MSG_LIMIT; k++)
+                                // {
+                                //     user_buffer[j][k]=0; 
+                                // }
+                                memset (user_buffer[j], 0, MSG_LIMIT);
+                                user_buffer_len[j] = 0;  
                             }
-                        /*
+
+#ifdef _DEBUG
                         std::cout << "\n!!! 2.5 !!!\n";
-                        std::cout << ">> b_len: " << user_buffer_len[i] << " buff_i: \"" << user_buffer[i] << "\"" << std::endl; */
+                        std::cout << ">> b_len: " << user_buffer_len[i] << " buff_i: \"" << user_buffer[i] << "\"" << std::endl;
+#endif
+
                         memcpy (user_buffer[i], recv_buffer + (recv_len - shift), shift);
                         user_buffer_len[i] = shift;
                         recv_len = 0U;
+                        memset (recv_buffer, 0, MSG_LIMIT);
                     }
-                /*
+                    
+                    else
+                    {
+                        recv_len = 0U;
+                        memset (recv_buffer, 0, MSG_LIMIT);  
+                    }
+#ifdef _DEBUG
                 std::cout << "\n!!! 3 !!!\n";
                 std::cout << ">> b_len: " << user_buffer_len[i] << " buff_i: \"" << user_buffer[i] << "\"" << std::endl;
                 std::cout << ">> rcv_len: " << recv_len << " recv: \"" << recv_buffer << "\"" << std::endl;
-                */
+#endif
                 } while (1);
             } // end else
         }  // end for Event [i]
